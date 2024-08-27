8eee4f55fa898c6dfd009270fe7e2809487d2686
Silanab <silanabot01@gmail.com>
2024-08-27 14:51:20 +0100

    Create blocklist.js

---
+++ plugins/blocklist.js
@@ -0,0 +1,21 @@
+// WhatsApp: https://whatsapp.com/channel/0029VacrIfU3LdQdklKFR419
+
+let handler = async (m, { conn, usedPrefix }) => {
+    await conn.fetchBlocklist().then(async data => {
+        let txt = *「 قائمة الأرقام المحظورة 」*\n\n*العدد الكلي:* ${data.length}\n\n┌─\n
+        for (let i of data) {
+            txt += ├🚷 @${i.split("@")[0]}\n
+        }
+        txt += "└────"
+        return conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) })
+    }).catch(err => {
+        console.log(err);
+        throw 'لا يوجد أرقام محظورة!'
+    })
+}
+
+handler.tags = ['owner']
+handler.help = ['blocklist']
+handler.command = /^(blocklist|listblock)$/i
+handler.owner = true
+export default handler
